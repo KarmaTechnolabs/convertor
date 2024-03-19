256e6dbb97b628d12cf03d191d9e4045fc8a4a8a
Dhara <Dhara.karmatechnolabs@gmail.com>
2024-03-19 11:03:47 +0530

    Add new unit

---
+++ Converter/Conversions/Resistance.swift
@@ -0,0 +1,8 @@
+//
+//  Resistance.swift
+//  Converter
+//
+//  Created by Karma on 18/03/24.
+//
+
+import Foundation
---
+++ Converter/Conversions/Sound.swift
@@ -0,0 +1,8 @@
+//
+//  Sound.swift
+//  Converter
+//
+//  Created by Karma on 18/03/24.
+//
+
+import Foundation
